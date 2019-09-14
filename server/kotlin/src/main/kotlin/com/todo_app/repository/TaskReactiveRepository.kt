03e8fb5604c379ae5aa090eae85478d666ca8077
Unknown <cesarlp.ng@gmail.com>
2019-09-14 11:31:44 -0300

    Cleanup: removed kotlin backend

--- server/kotlin/src/main/kotlin/com/todo_app/repository/TaskReactiveRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.todo_app.repository
-
-import com.todo_app.model.Task
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TaskReactiveRepository : ReactiveMongoRepository<Task, String>
